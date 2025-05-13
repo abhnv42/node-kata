c05093b5b292eae221c74bf9787979076b9d7020
Abhinav <abhnvashgpta@gmail.com>
2025-05-13 22:45:11 +0530

    GREEN: single number test passed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 export default function add(numbers) {
 	let numbersArray = getNumbers(numbers);
 	if(numbersArray.length === 0) return 0;
+	return numbersArray.pop();
 }
 
 function getNumbers(numbers) {
 	if(numbers.length === 0) return [];
+	return [Number(numbers)];
 }
