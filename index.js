cf2d600d21a6b0d6ea5c502abdb8e5a3e2fc71cb
Abhinav <abhnvashgpta@gmail.com>
2025-05-13 22:47:35 +0530

    REFACTOR: used Array.reduce to calculate sum

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default function add(numbers) {
 	let numbersArray = getNumbers(numbers);
 	if(numbersArray.length === 0) return 0;
-	return numbersArray.pop();
+	return numbersArray.reduce((acc, number) => acc + number, 0);
 }
 
 function getNumbers(numbers) {
